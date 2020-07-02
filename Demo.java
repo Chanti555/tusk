1aefbf5eca22af6921c92df05310b353c872a7bf
Chanti555 <56964483+Chanti555@users.noreply.github.com>
2020-07-02 08:42:03 +0530

    welcome text

--- Demo.java
+++ Demo.java
@@ -1,18 +1,17 @@
 class Demo
 {
+  /*
   static int add(int x,int y)
   {
     return x+y;
-  }
-  static int cube(int n)
-  {
-    return n*n*n;
   }
   static int sub(int x,int y)
   {
     return x-y;
   }
+  */
   public static void main(String args[])
   {
+    System.out.println("Welcome to Git");
   }
 }
